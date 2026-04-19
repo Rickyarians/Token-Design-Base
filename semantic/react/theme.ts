71394d5843f3accc55c14cc750ccc849933e2647
mamang-color <mamang-color@uayuex.com>
2026-04-19 19:10:00 +0000

    auto build tokens

--- semantic/react/theme.ts
+++ semantic/react/theme.ts
@@ -3,8 +3,12 @@ export const theme = {
     "black": "#000000",
     "blue": "#2364aa",
     "coklat": "#efd9ce",
+    "darkcyan": "#3c887e",
     "grey": "#bdbdbd",
+    "greynew": "#bbddbbdd",
     "purple": "#b79ced",
+    "skyaqua": "#3c887e",
+    "springgreen": "#0fff95",
     "tropicalTeal": "#73bfb8",
     "white": "#ffffff",
     "yellow": "#fec601"
